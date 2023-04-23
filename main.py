a884da949162f4212f75ebddcbbdaf30bc004ca3
Abhimanyu Singh <abhimanyu.cpp@gmail.com>
2023-04-23 13:34:34 -0700

    load data and email

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import datetime
+import smtplib
+from email.message import EmailMessage
+import os
 
+def sendEmail(to, sub, msg):
+    print(f"email to {to} \nsend with subject: {sub}\n message: {msg}")
+    email = EmailMessage()
+    email['from'] = 'Abhimanyu Singh'
+    email['to'] = f"{to}"
+    email['subject'] = f'{sub}'
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    email.set_content(f'{msg}')
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('Email','password')
+        smtp.send_message(email)
+        print("Email send")
+    pass
+if __name__ == "__main__":
+    df = pd.read_excel("data.xlsx")
+    print(df)
+    today = datetime.datetime.now().strftime("%d-%m")
+    #print(type(today))
+    update = []
+    yearnow =  datetime.datetime.now().strftime("%Y")
+    #print(yearnow)
+    for index, item in df.iterrows():
+        #print(index,item['birthday'])
+        bday = item['Birthday'].strftime("%d-%m")
+        #print(type(bday))
+        if(bday == today) and yearnow not in str(item["Year"]):
+            sendEmail(item['Email'] ,"Happy BIrthday "+item["Name"], item['message'])
+            update.append(index)
+    for i in update:
+        yr = df.loc[i, 'Year']
+        #print(yr)
+        df.loc[i,'Year'] = f"{yr}, {yearnow}"
+        #print((df.loc[i, 'Year'])
+    #print(df)
+    df.to_excel("data.xlsx", index=False)
